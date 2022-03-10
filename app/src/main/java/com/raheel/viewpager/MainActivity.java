f0f9ea98c196bcf44d0de8cecd25f700c18e7016
mraheeldev <m.raheeldev@gmail.com>
2022-03-10 11:24:11 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/viewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.raheel.viewpager;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+    }
+
+
+}
